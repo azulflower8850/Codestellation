d3b0e699015d4398207b0dcb4c6a7351b7a7dfd2
Nathan <nathaniel.yee@students.olin.edu>
2015-11-15 02:50:18 -0500

    integrated indicio api and twitter api

--- learntwython.py
+++ learntwython.py
@@ -2,8 +2,8 @@
 from twython import Twython
 
 #Setting these as variables will make them easier for future edits
-app_key = "H9mapu9yLJR9dAh3T3qkU4YWB"
-app_secret = "NImr0l7PX9pzu1as55KZj7RYbSmZnLF7svC8VlDW3rVJmX89xF"
+app_key = "BgJM8zAWf6LF5KRqn0PlNJWTs"
+app_secret = "YHui1c1TIS5gWhh3Kn1CjZOZmlem1t5r1l8nJcEMvbOuOpR3yR"
 oauth_token = "4190375567-3fELjcR6GibnpXr4yOtJA62iw5SCIenNUALJPoq"
 oauth_token_secret = "9Tv2bWSvwCEF59cN367IZczC0LqFe5NOLPpkF22yTTK3R"
 
@@ -12,7 +12,7 @@ twitter = Twython(app_key, app_secret, oauth_token, oauth_token_secret)
 #The above should just be a single line, without the break
 
 #The obligatory first status update to test
-# twitter.update_status(status="Hello, world!")
+# twitter.update_status(status="testing")
 
 
 
@@ -24,31 +24,40 @@ twitter = Twython(app_key, app_secret, oauth_token, oauth_token_secret)
 # file.write(str(search))
 
 # file.close()
-
+import indicoio
+indicoio.config.api_key = '580dc401830e72a98d4834bafe5b7d7c'
 
 
 #Twitter stream
-# from twython import TwythonStreamer
-# class MyStreamer(TwythonStreamer):
-#     def on_success(self, data):
-#         if 'text' in data:
-#             print data['text'].encode('utf-8')
+from twython import TwythonStreamer
+class MyStreamer(TwythonStreamer):
+    def on_success(self, data):
+        if 'text' in data:
+        	x = data['text'].encode('utf-8')
+        	sentiment = (indicoio.sentiment_hq(x))
+        	print x
+        	print sentiment
+            # print data['text'].encode('utf-8')
 
-#     def on_error(self, status_code, data):
-#         print status_code
+    def on_error(self, status_code, data):
+        # print status_code
+        x = status_code
+        sentiment = (indicoio.sentiment_hq(x))
+        print x
+        print sentiment
 
-#         # Want to stop trying to get data because of the error?
-#         # Uncomment the next line!
-#         # self.disconnect()
+        # Want to stop trying to get data because of the error?
+        # Uncomment the next line!
+        # self.disconnect()
 
 
-# stream = MyStreamer(app_key, app_secret, oauth_token, oauth_token_secret)
-# stream.statuses.filter(track='python')
+stream = MyStreamer(app_key, app_secret, oauth_token, oauth_token_secret)
+stream.statuses.filter(track='codestellation')
 
 
+#read a user's timeline
+# user_tweets = twitter.get_user_timeline(screen_name='soylentbro', include_rts=True)
 
-user_tweets = twitter.get_user_timeline(screen_name='soylentbro', include_rts=True)
-
-for tweet in user_tweets:
-    tweet['text'] = Twython.html_for_tweet(tweet)
-    print tweet['text']
+# for tweet in user_tweets:
+#     tweet['text'] = Twython.html_for_tweet(tweet)
+#     print tweet['text']
